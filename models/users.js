e0449b125ce04a1bd87b989129868f1f49cbcc46
arooj-ilyas <112420017+arooj-ilyas@users.noreply.github.com>
2022-11-22 15:21:44 +0000

    edits updated

--- models/users.js
+++ models/users.js
@@ -4,7 +4,8 @@ const { query } = require("../db/index");
 // create async function which will return all users in the db
 async function getAllUsers() {
 	let result = await query(`SELECT username FROM skamtable`);
-	return result;
+	let usernameResult = result.rows;
+	return usernameResult;
 }
 
 module.exports = {

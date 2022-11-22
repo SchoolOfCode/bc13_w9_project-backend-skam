df2799739cb295a4473b3bd9d2d85af66326863f
arooj-ilyas <112420017+arooj-ilyas@users.noreply.github.com>
2022-11-22 16:25:49 +0000

    patch request set

--- models/users.js
+++ models/users.js
@@ -8,6 +8,24 @@ async function getAllUsers() {
 	return usernameResult;
 }
 
+// create an async function which adds new to existing user
+async function updateUserUsername(id, update) {
+	let result = await query(
+		`UPDATE skamtable
+        SET username = $1
+        WHERE user_id = $2
+        RETURNING *;`,
+		[update.username, id]
+	);
+	let updatedUsername = result;
+	//console.log(id);
+	//console.log(result);
+	return updatedUsername;
+}
+
+// create an async function which posts a new user
+
 module.exports = {
 	getAllUsers,
+	updateUserUsername,
 };
--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,18 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllUsers } = require("../models/users");
+const { getAllUsers, updateUserUsername } = require("../models/users");
 
 router.get("/", async function (req, res) {
 	const users = await getAllUsers();
 	res.json({ success: true, payload: users });
+});
+
+router.patch("/:id", async function (req, res) {
+	const data = req.body;
+	console.log(data);
+	const updateUsername = await updateUserUsername(req.params.id, data);
+	res.json({ success: true, payload: updateUsername });
 });
 
 module.exports = router;

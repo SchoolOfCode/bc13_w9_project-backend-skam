498c5308c614b1fc84f4bb6d9cc774538a63b55f
arooj-ilyas <112420017+arooj-ilyas@users.noreply.github.com>
2022-11-22 13:45:12 +0000

    npms downloaded

--- db/index.js
+++ db/index.js
@@ -1,8 +1,15 @@
-const { Pool } = require('pg')
-const pool = new Pool({ connectionString: process.env.POSTGRES_CONNECTION_URL, });
+import { Pool } from "pg";
+const pool = new Pool({
+	connectionString: process.env.POSTGRES_CONNECTION_URL,
+});
 
 module.exports = {
-    query: function(text, params) {
-        return pool.query(text, params)
-    }
-}
+	//query key in our export object where a value of the query key is a function
+	//query is an anonymous function
+	query: function (text, params) {
+		//text: SQL query
+		//params: parameterized queries (prepared statements)
+		return pool.query(text, params);
+		//query function grabs what it needs & returns the text and params to the pool
+	},
+};

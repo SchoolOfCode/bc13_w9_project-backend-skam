4c85479e8ace7b289077130abaa0ea44dbd29142
arooj-ilyas <112420017+arooj-ilyas@users.noreply.github.com>
2022-11-23 11:49:24 +0000

    test skeleton made

--- routes/users.test.js
+++ routes/users.test.js
@@ -9,13 +9,6 @@
 //    success is true
 //    outer level test -> payload is an array of objects
 
-// Test that you can get users by email, phone, spoken language, programming language, looking to teach, looking to learn, looking to work, looking to hire, looking to collab
-//    For each of these
-//    response code = 200
-//    success = true
-//      outer level test -> payload is an array of objects
-//      second level test -> object contain whatever the search parameter was (e.g. email sophie@mail.com)
-
 import { expect, test } from "@jest/globals";
 import app from "../app.js";
 
@@ -32,4 +25,16 @@ test("Get all the users in the database", async function () {
 	let response = await request.get("/users");
 	expect(response.status).toBe(200);
 	console.log(response.body.payload);
+	expect(response.body.success).toStrictEqual(true);
+	expect(response.body.payload).toBeInstanceOf(Array);
+});
+
+// Test that you can get users by email, phone, spoken language, programming language, looking to teach, looking to learn, looking to work, looking to hire, looking to collab
+//    For each of these
+//    response code = 200
+//    success = true
+//      outer level test -> payload is an array of objects
+//      second level test -> object contain whatever the search parameter was (e.g. email sophie@mail.com)
+test("The email parameter searched gives the correct user from the db", async function () {
+	let response = await request.get("/users");
 });

4b587b682723a3e20fa3c33f99a490b55f08c0d8
arooj-ilyas <112420017+arooj-ilyas@users.noreply.github.com>
2022-11-23 12:51:00 +0000

    updated cors to allow reqs from everywhere with *

--- app.js
+++ app.js
@@ -12,10 +12,13 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 
-app.use("/users", router);
+//tell the app to use cors on the local host where react is running
+//cors checks if the frontend has permission to communicate with the backend
+//if you do cors("http://localhost:3001") it means you can be specific of who can fetch your data
+//for now, use * to allow it to be accessed from everywhere
+app.use(cors("*"));
 
-//tell the app to use cors
-app.use(cors());
+app.use("/users", router);
 
 app.listen(PORT, function () {
 	console.log(`Server listening on port ${PORT}`);

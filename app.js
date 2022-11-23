07e627a15be9734e74b1f228d1a0639af56902f4
arooj-ilyas <112420017+arooj-ilyas@users.noreply.github.com>
2022-11-23 11:48:44 +0000

    cors middleware added

--- app.js
+++ app.js
@@ -2,15 +2,20 @@
 // const Express = pkg;
 // import pkg from "express";
 import Express from "express";
-
 import router from "./routes/users.js";
+
+//import cors to allow front end to fetch data from backend without getting blocked
+import cors from "cors";
 
 const app = Express();
 const PORT = process.env.PORT;
 
-// app.use(json());
+//app.use(json());
 
 app.use("/users", router);
+
+//tell the app to use cors
+app.use(cors());
 
 app.listen(PORT, function () {
 	console.log(`Server listening on port ${PORT}`);
